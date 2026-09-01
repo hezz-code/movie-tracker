5293be0a32585421c48550ca4a439681718290e4
Hezz2223 <henry.reis111@gmail.com>
2026-09-01 11:50:51 +0100

    Added movie search

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from tmdb_api import movie_search
 
+# user enters a movie name and its searched for using the api
 movie_name = input("Enter a movie: ")
 results = movie_search(movie_name)
 
+# checking if the movie exists
 if results and results["results"]:
-    movie = results["results"][0]
+    print("\nSearch Results: \n")
+    for i, movie in enumerate(results["results"]):#
+        # prints the number, title, release date
+        print(f"{i}. {movie['title']} ({movie['release_date'][:4]})")
+        # prints the rating, vote average
+        print(f"Rating: {movie['vote_average']}")
+        print()
 
-    print("")
-    print("Title: ", movie["title"])
-    print("Release Date: ", movie["release_date"])
-    print("Rating: ", movie["vote_average"])
-    print("Overview: ", movie["overview"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 else:
     print("Movie not found.")
